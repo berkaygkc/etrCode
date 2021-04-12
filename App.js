9d07d858117a77ad94e2be95d20e9e7ad34fa8e6
Berkay Gökçe <berkaygkc7@gmail.com>
2021-04-12 20:45:52 +0300

    Kaynak kodlar eklendi.

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
+const test = '156489785641987489654198;'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <QRCode
+        value='${test}'
+      />
     </View>
   );
 }
